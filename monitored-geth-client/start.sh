1d1a18ec0ff20d154fc34fca3b736f90df44d42a
Xi Xue Jia <xixuejia@gmail.com>
2016-12-21 13:08:48 +0800

    created 2 new accounts

--- monitored-geth-client/start.sh
+++ monitored-geth-client/start.sh
@@ -4,7 +4,7 @@ cd /root/eth-net-intelligence-api
 perl -pi -e "s/XXX/$(hostname)/g" app.json
 /usr/bin/pm2 start ./app.json
 sleep 3
-/usr/bin/geth --datadir=~/.ethereum/devchain init "/root/files/genesis.json"
+/usr/bin/geth --datadir=~/.ethereum init "/root/files/genesis.json"
 sleep 3
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
 GETH_OPTS=${@/XXX/$BOOTSTRAP_IP}
